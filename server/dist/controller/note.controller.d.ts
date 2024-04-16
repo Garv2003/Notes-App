be9fa38e7580bfc7128962c29bfa6b9bd8bc2bc9
Garv2003 <aggarwalgarv0505@gmail.com>
2024-04-16 20:42:38 +0530

    FINAL TOUCH

--- server/dist/controller/note.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-declare const GetUserNotes: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-declare const CreateNote: (req: Request, res: Response) => Promise<void>;
-declare const GetNoteById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-declare const UpdateNote: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-declare const DeleteNote: (req: Request, res: Response) => Promise<void>;
-export { GetUserNotes, CreateNote, GetNoteById, UpdateNote, DeleteNote };
